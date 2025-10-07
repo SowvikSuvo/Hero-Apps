4bf81cfd1af5ae24cb0677e22b42441a54e73a2d
SowvikSuvo <skarmoker44@gmail.com>
2025-10-08 00:43:54 +0600

    Added navbar,hero section footer

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,19 +3,28 @@ import App from "../App";
 import Home from "../Pages/Home";
 import Apps from "../Pages/Apps";
 import Installation from "../Pages/Installation";
+import RootLayout from "../Layouts/RootLayout";
+import ErrorPage from "../Pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
-  },
-  {
-    path: "/apps",
-    element: <Apps></Apps>,
-  },
-  {
-    path: "/installation",
-    element: <Installation></Installation>,
+    element: <RootLayout></RootLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: "/apps",
+        element: <Apps></Apps>,
+      },
+      {
+        path: "/installation",
+        element: <Installation></Installation>,
+      },
+    ],
   },
 ]);
 
